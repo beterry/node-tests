9caa882c61f770ebaa9bc04a0b3cd331d9769afb
Ben Terry <benterry@Bens-MacBook.local>
2019-01-30 12:15:30 -0500

    added async testing and expect support

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,41 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
   let res = utils.add(33,11);
-  if (res !== 44){
-    throw new Error(`Expected 44 but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
+  // if (res !== 44){
+  //   throw new Error(`Expected 44 but got ${res}`);
+  // }
 });
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4,3,(sum)=>{
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  })
+})
 
 it('should square a number', () => {
   let res = utils.square(5);
-  if (res !== 25){
-    throw new Error(`Expected 25 but got ${res}`);
-  }
+  expect(res).toBe(25).toBeA('number');
+  // if (res !== 25){
+  //   throw new Error(`Expected 25 but got ${res}`);
+  // }
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(3,(res)=>{
+    expect(res).toBe(9).toBeA('number');
+    done();
+  })
+})
+
+it('should set users first and last name', () => {
+  let user = {};
+  let res = utils.setName(user,'Ben Terry');
+  expect(res).toInclude({
+    firstName: 'Ben',
+    lastName: 'Terry'
+  }).toBeA('object');
 });
