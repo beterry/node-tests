d73c28c24ebdb407a6808a1ed732fe2ffea22b82
Ben Terry <benterry@Bens-MacBook.local>
2019-01-30 15:34:43 -0500

    added describe tags to organize testing

--- server/server.test.js
+++ server/server.test.js
@@ -3,24 +3,31 @@ const expect = require('expect');
 
 let app = require('./server.js').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res)=>{
-      expect(res.body).toInclude({
-        error: 'Page not found'
-      });
-    })
-    .end(done);
-});
 
-it('should contain my name', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res)=>{
-      expect(res.body).toInclude({name: 'Ben', age:25});
-    })
-    .end(done);
+describe('SERVER', ()=>{
+  describe('GET /', ()=>{
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res)=>{
+          expect(res.body).toInclude({
+            error: 'Page not found'
+          });
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /users', ()=>{
+    it('should contain my name', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res)=>{
+          expect(res.body).toInclude({name: 'Ben', age:25});
+        })
+        .end(done);
+    });
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,41 +1,43 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-  let res = utils.add(33,11);
-  expect(res).toBe(44).toBeA('number');
-  // if (res !== 44){
-  //   throw new Error(`Expected 44 but got ${res}`);
-  // }
-});
+describe('utils', () => {
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4,3,(sum)=>{
-    expect(sum).toBe(7).toBeA('number');
-    done();
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      let res = utils.add(33,11);
+      expect(res).toBe(44).toBeA('number');
+    });
+  });
+
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4,3,(sum)=>{
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    })
   })
-})
 
-it('should square a number', () => {
-  let res = utils.square(5);
-  expect(res).toBe(25).toBeA('number');
-  // if (res !== 25){
-  //   throw new Error(`Expected 25 but got ${res}`);
-  // }
-});
+  it('should square a number', () => {
+    let res = utils.square(5);
+    expect(res).toBe(25).toBeA('number');
+    // if (res !== 25){
+    //   throw new Error(`Expected 25 but got ${res}`);
+    // }
+  });
 
-it('should async square a number', (done) => {
-  utils.asyncSquare(3,(res)=>{
-    expect(res).toBe(9).toBeA('number');
-    done();
+  it('should async square a number', (done) => {
+    utils.asyncSquare(3,(res)=>{
+      expect(res).toBe(9).toBeA('number');
+      done();
+    })
   })
-})
 
-it('should set users first and last name', () => {
-  let user = {};
-  let res = utils.setName(user,'Ben Terry');
-  expect(res).toInclude({
-    firstName: 'Ben',
-    lastName: 'Terry'
-  }).toBeA('object');
+  it('should set users first and last name', () => {
+    let user = {};
+    let res = utils.setName(user,'Ben Terry');
+    expect(res).toInclude({
+      firstName: 'Ben',
+      lastName: 'Terry'
+    }).toBeA('object');
+  });
 });
